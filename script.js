a543acb33e961ffb6955facc22b839fa6c79c9df
Renato Lins Jr <80003602+jucox@users.noreply.github.com>
2022-07-25 17:42:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-const menuButton = document.querySelector('#menu')
-
 function openMenu() {
     document.querySelector('.nav-header').style.display = 'flex'
     document.querySelector('.nav-header').style.left = '0%'
-    menuButton.style.display = 'none'
+    document.querySelector('#menu').style.display = 'none'
     document.querySelector('#close').style.display = 'flex'
     document.querySelector('body').style.overflowY = 'hidden'
 }
 
 function closeMenu() {
     document.querySelector('.nav-header').style.display = 'none'
-    menuButton.style.display = 'flex'
+    document.querySelector('#menu').style.display = 'flex'
     document.querySelector('#close').style.display = 'none'
     document.querySelector('body').style.overflowY = 'scroll'
 }
-
